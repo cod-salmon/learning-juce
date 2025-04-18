b6e494010e33e7e4d45c7fc4b68b18cc58689daf
cod-salmon <not_jean-paul_sartre@proton.me>
2025-04-18 13:36:10 +0200

    audio device manager tutorial

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -7,7 +7,7 @@
 */
 
 #include <JuceHeader.h>
-#include "HandlingMidiEventsTutorial.h"
+#include "AudioDeviceManagerTutorial.h"
 
 class Application    : public juce::JUCEApplication
 {
@@ -15,12 +15,12 @@ public:
     //==============================================================================
     Application() = default;
 
-    const juce::String getApplicationName() override       { return "HandlingMidiEventsTutorial"; }
+    const juce::String getApplicationName() override       { return "AudioDeviceManagerTutorial"; }
     const juce::String getApplicationVersion() override    { return "1.0.0"; }
 
     void initialise (const juce::String&) override
     {
-        mainWindow.reset (new MainWindow ("HandlingMidiEventsTutorial", new MainContentComponent, *this));
+        mainWindow.reset (new MainWindow ("AudioDeviceManagerTutorial", new MainContentComponent, *this));
     }
 
     void shutdown() override                         { mainWindow = nullptr; }
